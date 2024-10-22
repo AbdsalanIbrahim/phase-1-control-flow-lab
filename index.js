d60bababfd105c1be24b31082740fe794101d2c9
Abdisalan Ibrahim <crgmelo22@gmail.com>
2024-10-22 07:13:57 +0300

    .

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-// 1. Function for scuberGreetingForFeet
 function scuberGreetingForFeet(someValue) {
   if (someValue <= 400) {
-    return "This one is on me!";
-  } else if (someValue > 2000 && someValue < 2500) {
-    return "I will gladly take your thirty bucks.";
-  } else if (someValue >= 2500) {
-    return "No can do.";
+    return 'This one is on me!';
+  } else if (someValue > 400 && someValue <= 2000) {
+    return 'That will be $30.';
+  } else if (someValue > 2000 && someValue <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
   }
 }
 
-// 2. Function for ternaryCheckCity
 function ternaryCheckCity(city) {
-  return city === "NYC" ? "Ok, sounds good." : "No go.";
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-// 3. Function for switchOnCharmFromTip
 function switchOnCharmFromTip(tip) {
   switch (tip) {
-    case "generous":
-      return "Thank you so much.";
-    case "not as generous":
-      return "Thank you.";
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
     default:
-      return "Bye.";
+      return 'Bye.';
   }
 }
+
+module.exports = {
+  scuberGreetingForFeet,
+  ternaryCheckCity,
+  switchOnCharmFromTip
+};
